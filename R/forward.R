c33b0220c216f4029c376f9d9ea19729281911e6
Jan-Ole Koslik <janole.koslik@gmail.com>
2023-08-24 08:41:34 +0200

    .

---
+++ R/forward.R
@@ -0,0 +1,17 @@
+#' R-Wrapper function for forward algorithm in CPP for periodic variation
+#'
+#' @param delta Initial distribution
+#' @param allprobs allprobs matrix
+#' @param Gamma1 Gamma matrix in LD condition (array)
+#' @param Gamma2 Gamma matrix in DD condition (array)
+#' @param startDD startindex of DD condition
+#' @param X data
+#'
+#' @return
+#' @export
+#'
+#' @examples
+forward_CPP = function(delta, allprobs, Gamma1, Gamma2, startDD, X){
+  l = forward_cpp(allprobs, delta, Gamma[,,,1], Gamma[,,,2], startDD, X_k$tod-1)
+  return(l)
+}

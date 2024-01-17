67d0147789bad0699686147e2c0f1d0f3e956972
Jan-Ole Koslik <janole.koslik@gmail.com>
2024-01-17 15:42:43 +0100

    Updating documentation for semigroup

--- R/semigroup.R
+++ R/semigroup.R
@@ -4,7 +4,7 @@
 #' When observing data at time points \eqn{t_1, \dots, t_n} the transition probabilites between \eqn{t_i} and \eqn{t_{i+1}} are caluclated as \cr \cr
 #' \eqn{\Gamma(\Delta t_i) = \exp(Q \Delta t_i)}, \cr \cr
 #' where \eqn{\exp()} is the matrix exponential. The mapping \eqn{\Gamma(\Delta t)} is also called the Markov semigroup.
-#' This function calculated all transition matrices based on a given generator and time differences.
+#' This function calculates all transition matrices based on a given generator and time differences.
 #'
 #' @param Q Infinitesimal generator matrix of the continuous-time Markov chain of dimension c(N,N)
 #' @param times Time differences between observations of length n-1 when based on n observations

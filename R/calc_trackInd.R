7bfbe116521212ee8d4b2c121386796f96c58469
Jan-Ole Koslik <101901166+janoleko@users.noreply.github.com>
2024-04-30 11:52:29 +0200

    adding a function that calculates trackInd

---
+++ R/calc_trackInd.R
@@ -0,0 +1,28 @@
+#' Calculate the index of the first observation of each track based on an ID variable
+#' 
+#' Function to conveniently calculate the trackInd variable that is needed when fitting a model to longitudinal data with multiple tracks.
+#' 
+#' Caution: This function should NEVER be used INSIDE the likelihood function, as it will drastically slow down the computation speed!
+#' Instead, it should be called once and the result can then be passed as an argument to the likelihood function.
+#'
+#' @param ID ID variable of track IDs that is of the same length as the data to be analyzed
+#'
+#' @return A vector of indices of the first observation of each track which can be passed to the forward and forward_g to sum likelihood contributions of each track
+#' @export
+#'
+#' @examples
+#' uniqueID = c("Animal1", "Animal2", "Animal3")
+#' ID = rep(uniqueID, c(100, 200, 300))
+#' trackInd = calc_trackInd(ID)
+calc_trackInd = function(ID){
+  if(!is.vector(ID)){
+    stop("ID must be a vector")
+  }
+  uniqueID = unique(ID)
+  trackInd = rep(NA, length(uniqueID))
+  for(i in 1:length(uniqueID)){
+    ind = which(ID == uniqueID[i])
+    trackInd[i] = ind[1]
+  }
+  return(trackInd)
+}

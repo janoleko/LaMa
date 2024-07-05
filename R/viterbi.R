048401b655dee2f201a42efa0013962d55279301
Jan-Ole Koslik <101901166+janoleko@users.noreply.github.com>
2024-07-05 15:13:17 +0200

    Updating viterbi algorithm for multiple tracks

--- R/viterbi.R
+++ R/viterbi.R
@@ -1,8 +1,9 @@
 #' Viterbi algorithm for decoding states
 #'
-#' @param delta Initial or stationary distribution of length N
+#' @param delta Initial or stationary distribution of length N, or matrix of dimension c(k,N) for k independent tracks, if ID is provided
 #' @param Gamma Transition probability matrix of dimension c(N,N)
 #' @param allprobs Matrix of state-dependent probabilities/ density values of dimension c(n, N)
+#' @param ID Optional vector of k track IDs, if multiple tracks need to be decoded separately
 #'
 #' @return Vector of decoded states of length n
 #' @export
@@ -12,11 +13,18 @@
 #' Gamma = matrix(c(0.9, 0.1, 0.2, 0.8), nrow = 2, byrow = TRUE)
 #' allprobs = matrix(runif(200), nrow = 100, ncol = 2)
 #' states = viterbi(delta, Gamma, allprobs)
-viterbi = function(delta, Gamma, allprobs){
+viterbi = function(delta, Gamma, allprobs, ID = NULL){
   n = nrow(allprobs)
   N = ncol(allprobs)
-  Gammanew = array(Gamma, dim = c(N,N,n-1))
-  as.integer(viterbi_g_cpp(allprobs, delta, Gammanew))
+  
+  # inflating Gamma to use viterbi_g
+  if(!is.null(ID)){
+    Gamma = array(Gamma, dim = c(N,N,n))
+  } else{
+    Gamma = array(Gamma, dim = c(N,N,n-1))
+  }
+  
+  viterbi_g(delta, Gamma, allprobs, ID)
 }
 
 
--- R/viterbi_g.R
+++ R/viterbi_g.R
@@ -1,9 +1,11 @@
 #' Viterbi algorithm for decoding states of inhomogeneous HMMs
 #'
-#' @param delta Initial distribution of length N
+#' @param delta Initial distribution of length N, or matrix of dimension c(k,N) for k independent tracks, if ID is provided
 #' @param Gamma Array of transition probability matrices of dimension c(N,N,n-1), as in a time series of length n, there are only n-1 transitions. 
 #' If you provide an array of dimension c(N,N,n), the first slice will be ignored. \cr
+#' If you provide an ID vector, Gamma needs to be an array of dimension c(N,N,n), where n is the number of rows in allprobs. Then for each track the first transition matrix will be ignored.
 #' @param allprobs Matrix of state-dependent probabilities/ density values of dimension c(n, N)
+#' @param ID Optional vector of k track IDs, if multiple tracks need to be decoded separately
 #'
 #' @return Vector of decoded states of length n
 #' @export
@@ -18,14 +20,70 @@
 #' }
 #' allprobs = matrix(runif(200), nrow = 100, ncol = 2)
 #' states = viterbi_g(delta, Gamma, allprobs)
-viterbi_g = function(delta, Gamma, allprobs){
+viterbi_g = function(delta, Gamma, allprobs, ID = NULL){
   n = nrow(allprobs)
   N = ncol(allprobs)
   
-  if(dim(Gamma)[3]==n){
-    # not using the first slice of Gamma, if n slices are provided
-    Gamma = Gamma[,,-1]
+  # If ID is provided, several tracks need to be decoded separately
+  if(!is.null(ID)){
+    uID = unique(ID)
+    k = length(uID) # number of tracks
+    
+    if(is.vector(delta)){
+      delta = matrix(delta, nrow = k, ncol = length(delta), byrow = TRUE)
+    } else if(dim(delta)[1] != k){
+      stop("Delta needs to be either a vector of length N or a matrix of dimension c(k,N), matching the number tracks.")
+    }
+    
+    # initialising state vector
+    states = rep(NA, n)
+    
+    # loop over individual tracks
+    for(i in 1:length(uID)){
+      id_i = which(ID == uID[i])
+      
+      allprobs_i = allprobs[id_i, ]
+      if(dim(allprobs_i)[1] == 1) stop("All tracks must be at least of length 2.")
+      
+      if(dim(allprobs_i)[1] == 2){
+        # viterbi algorithm for track of length 2 only
+        Gamma_i = as.matrix(Gamma[,,id_i[-1]])
+        
+        xi = matrix(0, nrow = 2, ncol = N) 
+        foo = delta[i,] * allprobs_i[1, ]
+        xi[1, ] = foo / sum(foo)
+        
+        foo = apply(xi[1, ] * Gamma_i, 2, max) * allprobs[2, ]
+        xi[2, ] = foo / sum(foo)
+        
+        iv = numeric(2)
+        iv[2] = which.max(xi[2, ]) 
+        iv[1] = which.max(Gamma_i[, iv[2]] * xi[1, ])
+        
+        states[id_i] = iv
+        
+      } else{
+        # regurlar viterbi algorithm for this track
+        states[id_i] = viterbi_g_cpp(allprobs_i, delta[i,], Gamma[,,id_i[-1]])
+      }
+      
+    }
+    
+  } else{
+    if(!is.vector(delta)){
+      stop("If no ID is provided, delta needs to be a vector of length N.")
+    }
+    
+    if(dim(Gamma)[3]==n){
+      warning("Igoring the first slice of Gamma, as there are only n-1 transitions in a time series of length n.")
+      # not using the first slice of Gamma, if n slices are provided
+      Gamma = Gamma[,,-1]
+    }
+    
+    states = viterbi_g_cpp(allprobs, delta, Gamma)
   }
   
-  as.integer(viterbi_g_cpp(allprobs, delta, Gamma))
+  return(as.integer(states))
 }
+  
+

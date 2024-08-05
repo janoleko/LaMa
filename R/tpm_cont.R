e847391af7c156b9eea97140cb7fbf8d38cbd258
Jan-Ole Koslik <101901166+janoleko@users.noreply.github.com>
2024-08-05 13:55:06 +0200

    adding AD to tpm_cont

--- R/tpm_cont.R
+++ R/tpm_cont.R
@@ -8,9 +8,11 @@
 #'
 #' @param Q Infinitesimal generator matrix of the continuous-time Markov chain of dimension c(N,N)
 #' @param timediff Time differences between observations of length n-1 when based on n observations
+#' @param ad Logical, indicating whether automatic differentiation with RTMB should be used. Defaults to FALSE.
 #'
 #' @return An array of transition matrices of dimension c(N,N,n-1)
 #' @export
+#' @import RTMB
 #'
 #' @examples
 #' # building a Q matrix for a 3-state cont.-time Markov chain
@@ -23,6 +25,23 @@
 #' timediff = rexp(1000, 10)
 #'
 #' Gamma = tpm_cont(Q, timediff)
-tpm_cont = function(Q, timediff){
-  semigroup_cpp(Q, timediff)
+tpm_cont = function(Q, timediff, ad = FALSE){
+  
+  if(!ad) {
+    
+    Qube = semigroup_cpp(Q, timediff) # C++ version
+    
+  } else if(ad) { # ad version with RTMB
+    "[<-" <- ADoverload("[<-") # currently necessary
+    
+    n = length(timediff)
+    N = nrow(Q)
+    
+    Qube = array(NaN, dim = c(N, N, n))
+    
+    for(t in 1:n){
+      Qube[,,t] = RTMB::expm(Q * timediff[t]) # using RTMB::expm for AD
+    }
+  }
+  Qube
 }

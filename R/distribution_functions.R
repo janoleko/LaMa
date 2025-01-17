a8e53118bfd9d7436e73a78b940cb2135e0d6f05
Jan-Ole Koslik <101901166+janoleko@users.noreply.github.com>
2025-01-17 09:18:31 +0100

    adding some more densities

--- R/distribution_functions.R
+++ R/distribution_functions.R
@@ -19,7 +19,7 @@
 #'
 #' @examples 
 #' set.seed(1)
-#' x = rvm(1000, 0, 1)
+#' x = rvm(10, 0, 1)
 #' d = dvm(x, 0, 1)
 #' p = pvm(x, 0, 1)
 #' @name vm
@@ -28,11 +28,14 @@ NULL
 #' @rdname vm
 #' @export
 dvm = function(x, mu = 0, kappa = 1, log = FALSE) {
-  res = 1 / (2 * pi * RTMB::besselI(kappa, 0)) * exp(kappa * cos(x - mu))
+  logdens <- -log(2 * pi) - 
+    log(RTMB::besselI(kappa, 0)) + 
+    kappa * cos(x - mu)
+  
   if(log){
-    return(log(res))
+    return(logdens)
   } else{
-    return(res)
+    return(exp(logdens))
   }
 }
 
@@ -66,6 +69,58 @@ pvm = function(q, mu = 0, kappa = 1, from = NULL, tol = 1e-20) {
 #' @importFrom CircStats rvm
 rvm = function(n, mu = 0, kappa = 1, wrap = TRUE) {
   angles = CircStats::rvm(n, mu, kappa)
+  
+  # if generated angels should be wrapped, i.e. mapped to interval [-pi, pi], do so
+  if(wrap){
+    angles = (angles + pi) %% (2 * pi) - pi
+  }
+  angles
+}
+
+
+#' wrapped Cauchy distribution
+#' 
+#' Density and random generation for the wrapped Cauchy distribution.
+#' 
+#' @details
+#' The implementation of \code{dwrpcauchy} allows for automatic differentiation with \code{RTMB}. 
+#' \code{rwrpcauchy} is imported from \code{CircStats}.
+#'
+#' @param x vector of angles measured in radians at which to evaluate the density function.
+#' @param mu mean direction of the distribution measured in radians.
+#' @param rho concentration parameter of the distribution, must be in the interval from 0 to 1.
+#' @param log logical; if \code{TRUE}, densities are returned on the log scale.
+#' @param n number of observations. If \code{length(n) > 1}, the length is taken to be the number required.
+#' @param wrap logical; if \code{TRUE}, generated angles are wrapped to the interval [-pi, pi].
+#'
+#' @return \code{dwrpcauchy} gives the density and \code{rwrpcauchy} generates random deviates.
+#'
+#' @examples 
+#' set.seed(1)
+#' x = rwrpcauchy(10, 0, 1)
+#' d = dwrpcauchy(x, 0, 1)
+#' @name wrpcauchy
+NULL
+
+#' @rdname wrpcauchy
+#' @export
+dwrpcauchy <- function(x, mu = 0, rho, log = FALSE) {
+  logdens <- - log(2 * pi) + 
+    log(1 - rho^2) - 
+    log(1 + rho^2 - 2 * rho * cos(x - mu))
+  
+  if(log){
+    return(logdens)
+  } else{
+    return(exp(logdens))
+  }
+}
+
+#' @rdname wrpcauchy
+#' @export
+#' @importFrom CircStats rwrpcauchy
+rwrpcauchy = function(n, mu = 0, rho, wrap = TRUE) {
+  angles = CircStats::rwrpcauchy(n, mu, rho)
   
   # if generated angels should be wrapped, i.e. mapped to interval [-pi, pi], do so
   if(wrap){
@@ -133,6 +188,123 @@ rgamma2 = function(n, mean = 1, sd = 1) {
   shape = mean^2 / sd^2
   scale = sd^2 / mean
   stats::rgamma(n = n, shape = shape, scale = scale)
+}
+
+
+
+#' Skew normal distribution
+#' 
+#' Density, distribution function, quantile function and random generation for
+#' the skew normal distribution.
+#' 
+#' @details
+#' This implementation of \code{dskewnorm} allows for automatic differentiation with \code{RTMB} while the other functions are imported from the \code{sn} package.
+#'
+#' @param x,q vector of quantiles
+#' @param p vector of probabilities
+#' @param n number of observations. If \code{length(n) > 1}, the length is taken to be the number required.
+#' @param xi location parameter
+#' @param omega scale parameter, must be positive.
+#' @param alpha skewness parameter, +/- \code{Inf} is allowed.
+#' @param log logical; if \code{TRUE}, probabilities/ densities \eqn{p} are returned as \eqn{\log(p)}.
+#' @param ... additional parameters to be passed to the \code{sn} package functions for \code{pskewnorm} and \code{qskewnorm}.
+#'
+#' @return
+#' \code{dskewnorm} gives the density, \code{pskewnorm} gives the distribution function, \code{qskewnorm} gives the quantile function, and \code{rskewnorm} generates random deviates.
+#'
+#' @examples
+#' x = rskewnorm(1)
+#' d = dskewnorm(x)
+#' p = pskewnorm(x)
+#' q = qskewnorm(p)
+#' @name skewnorm
+NULL
+
+#' @rdname skewnorm
+#' @export
+#' @importFrom RTMB dnorm
+#' @importFrom RTMB pnorm
+dskewnorm <- function(x, xi = 0, omega = 1, alpha = 0, log = FALSE) {
+  z = (x - xi) / omega # standardised observation
+  
+  log_normal_density <- RTMB::dnorm(z, log = TRUE)
+  log_skew_component <- log(2) - log(omega) + log(RTMB::pnorm(alpha * z))
+
+  log_density = log_normal_density + log_skew_component
+  
+  if(log) {
+    return(log_density)
+  } else{
+    return(exp(log_density))
+  }
+}
+
+#' @rdname skewnorm
+#' @export
+#' @importFrom sn psn
+pskewnorm <- function(q, xi = 0, omega = 1, alpha = 0, ...) {
+  sn::psn(x = q, xi = xi, omega = omega, alpha = alpha, ...)
+}
+
+#' @rdname skewnorm
+#' @export
+#' @importFrom sn qsn
+qskewnorm <- function(p, xi = 0, omega = 1, alpha = 0, ...) {
+  sn::qsn(p = p, xi = xi, omega = omega, alpha = alpha, ...)
+}
+
+#' @rdname skewnorm
+#' @export
+#' @importFrom sn rsn
+rskewnorm <- function(n, xi = 0, omega = 1, alpha = 0) {
+  sn::rsn(n = n, xi = xi, omega = omega, alpha = alpha)
+}
+
+
+#' Dirichlet distribution
+#' 
+#' Density of the Dirichlet distribution.
+#' 
+#' @details
+#' This implementation of \code{ddirichlet} allows for automatic differentiation with \code{RTMB}.
+#'
+#' @param x vector or matrix of quantiles
+#' @param alpha vector or matrix of shape parameters
+#' @param log logical; if \code{TRUE}, densities \eqn{p} are returned as \eqn{\log(p)}.
+#'
+#' @return
+#' \code{ddirichlet} gives the density.
+#'
+#' @examples
+#' ddirichlet(c(0.2, 0.3, 0.5), c(1, 2, 3))
+#' @name dirichlet
+NULL
+
+#' @rdname dirichlet
+#' @export
+#' @import RTMB
+ddirichlet <- function(x, alpha, log = TRUE) {
+  # Check if x and alpha are vectors by checking if they have dimensions
+  if (is.null(dim(x))) x <- matrix(x, nrow = 1)
+  if (is.null(dim(alpha))) alpha <- matrix(alpha, nrow = 1)
+  
+  # Ensure x and alpha have the same number of columns
+  if (ncol(x) != ncol(alpha)) {
+    stop("x and alpha must have the same number of columns (categories).")
+  }
+  
+  # Compute log of the multivariate beta function B(alpha) for each row
+  log_B_alpha <- rowSums(lgamma(alpha)) - lgamma(rowSums(alpha))
+  
+  # Compute log density for each row
+  log_density <- rowSums((alpha - 1) * log(x)) - log_B_alpha
+  
+  # Return result based on the 'log' argument
+  if (log) {
+    return(log_density)
+  } else {
+    return(exp(log_density))
+  }
 }
 
 

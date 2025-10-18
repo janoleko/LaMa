c7d600fadc7f4b979028ef53ba56fb7b500ec6ba
Jan-Ole Koslik <101901166+janoleko@users.noreply.github.com>
2025-10-18 10:55:36 +0200

    cleaning up distributions

--- R/distribution_functions.R
+++ R/distribution_functions.R
@@ -27,25 +27,27 @@ NULL
 
 #' @rdname vm
 #' @export
-#' @importFrom RTMB besselI
+#' @importFrom RTMBdist dvm
 dvm = function(x, mu = 0, kappa = 1, log = FALSE) {
   
   # stable calculation of log(besselI(kappa, 0))
   # logI0 <- log(RTMB::besselI(kappa, 0, expon.scaled = TRUE)) + kappa
-  logI0 <- log(RTMB::besselI(kappa, 0))
-
-  logdens <- -log(2 * pi) - logI0 + kappa * cos(x - mu)
+  # logI0 <- log(RTMB::besselI(kappa, 0))
+  # 
+  # logdens <- -log(2 * pi) - logI0 + kappa * cos(x - mu)
+  # 
+  # # logdens <- -log(2 * pi) - 
+  # #   log(RTMB::besselI(kappa, 0)) + 
+  # #   kappa * cos(x - mu)
+  # 
+  # 
+  # if(log){
+  #   return(logdens)
+  # } else{
+  #   return(exp(logdens))
+  # }
   
-  # logdens <- -log(2 * pi) - 
-  #   log(RTMB::besselI(kappa, 0)) + 
-  #   kappa * cos(x - mu)
-  
-  
-  if(log){
-    return(logdens)
-  } else{
-    return(exp(logdens))
-  }
+  RTMBdist::dvm(x, mu = mu, kappa = kappa, log = log)
 }
 
 #' @rdname vm
@@ -273,54 +275,6 @@ qskewnorm <- function(p, xi = 0, omega = 1, alpha = 0, ...) {
 rskewnorm <- function(n, xi = 0, omega = 1, alpha = 0) {
   sn::rsn(n = n, xi = xi, omega = omega, alpha = alpha)
 }
-
-
-#' Dirichlet distribution
-#' 
-#' Density of the Dirichlet distribution.
-#' 
-#' @details
-#' This implementation of \code{ddirichlet} allows for automatic differentiation with \code{RTMB}.
-#'
-#' @param x vector or matrix of quantiles
-#' @param alpha vector or matrix of shape parameters
-#' @param log logical; if \code{TRUE}, densities \eqn{p} are returned as \eqn{\log(p)}.
-#'
-#' @return
-#' \code{ddirichlet} gives the density.
-#'
-#' @examples
-#' ddirichlet(c(0.2, 0.3, 0.5), c(1, 2, 3))
-#' @name dirichlet
-NULL
-
-#' @rdname dirichlet
-#' @export
-#' @import RTMB
-ddirichlet <- function(x, alpha, log = TRUE) {
-  # Check if x and alpha are vectors by checking if they have dimensions
-  if (is.null(dim(x))) x <- matrix(x, nrow = 1)
-  if (is.null(dim(alpha))) alpha <- matrix(alpha, nrow = 1)
-  
-  # Ensure x and alpha have the same number of columns
-  if (ncol(x) != ncol(alpha)) {
-    stop("x and alpha must have the same number of columns (categories).")
-  }
-  
-  # Compute log of the multivariate beta function B(alpha) for each row
-  log_B_alpha <- rowSums(lgamma(alpha)) - lgamma(rowSums(alpha))
-  
-  # Compute log density for each row
-  log_density <- rowSums((alpha - 1) * log(x)) - log_B_alpha
-  
-  # Return result based on the 'log' argument
-  if (log) {
-    return(log_density)
-  } else {
-    return(exp(log_density))
-  }
-}
-
 
 
 #' Zero-inflated density constructer
